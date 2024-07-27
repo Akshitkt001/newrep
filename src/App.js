913c56d4b75f8a7367624b2bc0fee5dbc7ad87d6
Akshit Kumar Tiwari <137613929+Akshitkt001@users.noreply.github.com>
2024-07-27 17:21:29 +0530

    Update deployment setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Data from "./Data";
 import { auth, provider } from "./firebase";
 import Header from "./Header";
@@ -18,24 +19,29 @@ function App() {
   };
 
   return (
-    <>
-      {
-        user ? (
-          <>
-            <Header photoURL={user.photoURL} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-            <div className="App">
-              <Sidebar onOptionSelect={setSelectedOption} />
-              <Data searchTerm={searchTerm} selectedOption={selectedOption} />
+    <Router basename="/newrep">
+      <>
+        {
+          user ? (
+            <>
+              <Header photoURL={user.photoURL} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+              <div className="App">
+                <Sidebar onOptionSelect={setSelectedOption} />
+                <Routes>
+                  <Route path="/" element={<Data searchTerm={searchTerm} selectedOption={selectedOption} />} />
+                  {/* Add more routes if needed */}
+                </Routes>
+              </div>
+            </>
+          ) : (
+            <div className="loginWrap">
+              <img src="https://logodownload.org/wp-content/uploads/2020/04/google-drive-logo-0-1.png" width="500" height="500" />
+              <button onClick={signIn}>Login</button>
             </div>
-          </>
-        ) : (
-          <div className="loginWrap">
-            <img src="https://logodownload.org/wp-content/uploads/2020/04/google-drive-logo-0-1.png" width="500" height="500" />
-            <button onClick={signIn}>Login</button>
-          </div>
-        )
-      }
-    </>
+          )
+        }
+      </>
+    </Router>
   );
 }
 
